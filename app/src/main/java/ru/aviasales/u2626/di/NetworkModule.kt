5ad16c84acb1f217ea977684fc0b08b8a52ffddb
Evgeny Shishkin <johnkil78@gmail.com>
2019-08-19 17:57:31 +0300

    Update dependencies

--- app/src/main/java/ru/aviasales/u2626/di/NetworkModule.kt
+++ app/src/main/java/ru/aviasales/u2626/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.Cache
 import okhttp3.OkHttpClient
+import okhttp3.brotli.BrotliInterceptor
 import ru.aviasales.u2626.OkhttpClientInitializer
 import java.io.File
 import javax.inject.Singleton
@@ -19,6 +20,7 @@ object NetworkModule {
     fun provideOkHttpClient(app: Application): OkHttpClient =
         OkHttpClient.Builder()
             .cache(httpCache(app))
+            .addInterceptor(BrotliInterceptor)
             .apply { OkhttpClientInitializer.initInterceptors(app, this) }
             .build()
 
